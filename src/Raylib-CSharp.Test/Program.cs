ea1504ebe4af7de1163e4bb351b4626d0ea95199
MrScautHD <65916181+MrScautHD@users.noreply.github.com>
2024-04-27 10:42:53 +0200

    Test

--- src/Raylib-CSharp.Test/Program.cs
+++ src/Raylib-CSharp.Test/Program.cs
@@ -4,6 +4,6 @@ Console.WriteLine("Hello, World!");
 Window.Init(1, 1, "Test");
 
 while (!Window.ShouldClose()) {
-   //Console.WriteLine(Window.GetPosition() + "TEST");
+   Console.WriteLine(Window.GetPosition() + "TEST");
 }
 

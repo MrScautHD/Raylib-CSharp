a54d2fc607c3b6884461f55898317f4db0adad11
MrScautHD <65916181+MrScautHD@users.noreply.github.com>
2024-05-08 19:07:58 +0200

    Working on Github Actions

--- src/Raylib-CSharp/Raylib.cs
+++ src/Raylib-CSharp/Raylib.cs
@@ -10,7 +10,7 @@ public static partial class Raylib {
 
     public const string Name = "raylib";
 
-    public static readonly Version Version = new(2, 0, 1);
+    public static readonly Version Version = new(2, 0, 3);
     public static readonly Version RlVersion = new(5, 0, 0);
 
     /// <summary>

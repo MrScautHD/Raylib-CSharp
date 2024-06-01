4105abedbb4209ec3675579d7eeb94f5e52d53d5
MrScautHD <65916181+MrScautHD@users.noreply.github.com>
2024-06-01 14:27:04 +0200

    Update Program.cs

--- src/Raylib-CSharp.Samples/Program.cs
+++ src/Raylib-CSharp.Samples/Program.cs
@@ -2,8 +2,8 @@
 using Raylib_CSharp.Samples.Core;
 
 List<ISample> samples = new List<ISample>();
-samples.Add(new CustomLogger());
 samples.Add(new BasicWindow());
+samples.Add(new CustomLogger());
 
 foreach (ISample sample in samples) {
     using (sample) {

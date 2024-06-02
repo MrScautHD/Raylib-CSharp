b59c3580e2d443a36899ed783389c1bd907dfb5f
MrScautHD <65916181+MrScautHD@users.noreply.github.com>
2024-06-02 21:36:24 +0200

    Update Raylib.cs

--- src/Raylib-CSharp/Raylib.cs
+++ src/Raylib-CSharp/Raylib.cs
@@ -10,7 +10,7 @@ public static class Raylib {
 
     public const string Name = "raylib";
 
-    public static readonly Version Version = new(4, 0, 3);
+    public static readonly Version Version = new(4, 1, 0);
     public static readonly Version RlVersion = new(5, 1, 0);
 
     /// <inheritdoc cref="RaylibApi.TakeScreenshot" />
